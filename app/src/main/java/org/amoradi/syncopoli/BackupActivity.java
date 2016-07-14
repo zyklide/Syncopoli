f2e4dbfd747cb63333ab5fcce6b618cfd0705873
Armin M <amoradi@fedoraproject.org>
2016-07-13 22:41:10 -0400

    removed included binaries for the sake of fdroid

--- app/src/main/java/org/amoradi/syncopoli/BackupActivity.java
+++ app/src/main/java/org/amoradi/syncopoli/BackupActivity.java
@@ -8,7 +8,9 @@ import android.app.FragmentTransaction;
 import android.content.ContentResolver;
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.PowerManager;
 import android.preference.PreferenceManager;
 import android.support.annotation.LayoutRes;
 import android.support.design.widget.FloatingActionButton;
@@ -27,15 +29,18 @@ import android.view.animation.AccelerateDecelerateInterpolator;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.List;
 
 public class BackupActivity extends AppCompatActivity implements IBackupHandler {
@@ -173,33 +178,101 @@ public class BackupActivity extends AppCompatActivity implements IBackupHandler 
         copyExecutable("ssh");
     }
 
-    public String copyExecutable(String filename) {
-        try {
-            File file = getFileStreamPath(filename);
+    public void copyExecutable(String filename) {
+        File file = getFileStreamPath(filename);
 
-            /*
-            if (file.exists()) {
-                return file.getAbsolutePath();
+        if (file.exists()) {
+            return;
+        }
+
+        new DownloadBinaryTask(this).execute(filename);
+    }
+
+    private class DownloadBinaryTask extends AsyncTask<String, Void, String> {
+
+        private Context context;
+        private PowerManager.WakeLock mWakeLock;
+        private String filePath;
+
+        public DownloadBinaryTask(Context context) {
+            this.context = context;
+        }
+
+        @Override
+        protected String doInBackground(String... filenames) {
+            InputStream input = null;
+            OutputStream output = null;
+            HttpURLConnection connection = null;
+
+            try {
+                URL url = new URL("https://amoradi.org/public/android/arm/" + filenames[0]);
+                filePath = context.getFileStreamPath(filenames[0]).getAbsolutePath();
+                connection = (HttpURLConnection) url.openConnection();
+                connection.connect();
+
+                // expect HTTP 200 OK, so we don't mistakenly save error report
+                // instead of the file
+                if (connection.getResponseCode() != HttpURLConnection.HTTP_OK) {
+                    return "Server returned HTTP " + connection.getResponseCode()
+                            + " " + connection.getResponseMessage();
+                }
+
+                // download the file
+                input = connection.getInputStream();
+                output = context.openFileOutput(filenames[0], Context.MODE_PRIVATE);
+
+                byte data[] = new byte[4096];
+                int count;
+                while ((count = input.read(data)) != -1) {
+                    // allow canceling with back button
+                    if (isCancelled()) {
+                        input.close();
+                        File f = new File(filePath);
+                        f.delete();
+                        return null;
+                    }
+                    output.write(data, 0, count);
+                }
+            } catch (Exception e) {
+                return e.toString();
+            } finally {
+                try {
+                    if (output != null)
+                        output.close();
+                    if (input != null)
+                        input.close();
+                } catch (IOException ignored) {
+                }
+
+                if (connection != null)
+                    connection.disconnect();
             }
-            */
+            return null;
+        }
 
-            InputStream ins = getAssets().open("arm/" + filename);
-            FileOutputStream outputStream = new FileOutputStream(file.getAbsolutePath());
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            // take CPU lock to prevent CPU from going off if the user
+            // presses the power button during download
+            PowerManager pm = (PowerManager) context.getSystemService(Context.POWER_SERVICE);
+            mWakeLock = pm.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK,
+                    getClass().getName());
+            mWakeLock.acquire();
+        }
 
-            byte[] buffer = new byte[ins.available()];
-            int bytes = ins.read(buffer);
-            outputStream.write(buffer, 0, bytes);
+        @Override
+        protected void onPostExecute(String result) {
+            mWakeLock.release();
 
-            outputStream.close();
-            ins.close();
-
-            File f = new File(file.getAbsolutePath());
-            f.setExecutable(true);
-
-            return f.getAbsolutePath();
-        } catch (IOException e) {
-            e.printStackTrace();
-            throw new RuntimeException(e);
+            if (result != null) {
+                Toast.makeText(context, "Download Error: " + result, Toast.LENGTH_LONG).show();
+                File f = new File(filePath);
+                f.delete();
+            } else {
+                File f = new File(filePath);
+                f.setExecutable(true);
+            }
         }
     }
 
